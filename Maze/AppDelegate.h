ff1b647639bb25e9253e928b08ac2a72b34ea7c3
Fraser King <qhan@live.ca>
2016-02-28 20:14:19 -0500

    Initial Commit

---
+++ Maze/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Maze
+//
+//  Created by Fraser King on 2016-02-28.
+//  Copyright © 2016 Fraser King. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Maze/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Maze
+//
+//  Created by Fraser King on 2016-02-28.
+//  Copyright © 2016 Fraser King. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
