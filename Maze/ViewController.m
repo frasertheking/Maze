707c39385f74adb85347736e5e1be153904c8705
Fraser King <qhan@live.ca>
2016-03-12 13:15:54 -0500

    initial maze solving stuff

--- Maze/ViewController.m
+++ Maze/ViewController.m
@@ -14,6 +14,10 @@
 @property (nonatomic) int n;
 @property (nonatomic) int m;
 @property (nonatomic) NSMutableArray* blockArray;
+@property (nonatomic) NSMutableArray* solArray;
+
+@property (nonatomic) int startRow;
+@property (nonatomic) int startCol;
 
 @end
 
@@ -30,7 +34,7 @@
 }
 
 - (void)createMaze {
-    [self removeSubviews];
+    [self removeSubviews:1];
     
     int blocks = 15;
     
@@ -39,8 +43,12 @@
     
     NSInteger padding = 25;
     self.blockArray = [NSMutableArray arrayWithCapacity:blocks*2+1];
+    self.solArray = [NSMutableArray arrayWithCapacity:blocks*2+1];
     for(int i=0; i<blocks*2+1; i++) {
         [self.blockArray addObject:[NSMutableArray arrayWithCapacity:blocks*2+1]];
+    }
+    for(int i=0; i<blocks*2+1; i++) {
+        [self.solArray addObject:[NSMutableArray arrayWithCapacity:blocks*2+1]];
     }
 
     DEMazeGenerator *maze = [[DEMazeGenerator alloc] initWithRow:self.n
@@ -76,30 +84,140 @@
                 }
                 
                 NSInteger size = (self.view.frame.size.width - padding * 2) / (self.m * 2 + 1);
+                BOOL first = YES;
                 
                 if (item[r][c] == 1 && !dontDraw) {
                     UIView *block = [[UIView alloc] initWithFrame:CGRectMake(r*size + padding, c*size + padding*5, size, size)];
-                    block.backgroundColor = [self getRandomColor];
+                    block.backgroundColor = [UIColor blueColor];
                     block.tag = 1;
+                    block.alpha = 0.5;
                     [self.view addSubview:block];
-                    [[self.blockArray objectAtIndex:r] insertObject:[NSNumber numberWithInt:1] atIndex:c];
+                    [[self.blockArray objectAtIndex:r] insertObject:[NSNumber numberWithInt:0] atIndex:c];
                 } else {
                     if (dontDraw) {
-                        [[self.blockArray objectAtIndex:r] insertObject:[NSNumber numberWithInt:2] atIndex:c];
+                        if (end) {
+                            [[self.blockArray objectAtIndex:r] insertObject:[NSNumber numberWithInt:2] atIndex:c];
+                        } else if (start) {
+                            self.startRow = r;
+                            self.startCol = c;
+                            [[self.blockArray objectAtIndex:r] insertObject:[NSNumber numberWithInt:1] atIndex:c];
+                            first = NO;
+                        }
                     } else {
-                        [[self.blockArray objectAtIndex:r] insertObject:[NSNumber numberWithInt:0] atIndex:c];
+                        [[self.blockArray objectAtIndex:r] insertObject:[NSNumber numberWithInt:1] atIndex:c];
                     }
                 }
+                [[self.solArray objectAtIndex:r] insertObject:[NSNumber numberWithInt:0] atIndex:c];
             }
         }
-        NSLog(@"Array:\n\n%@", self.blockArray);
+        [self drawSolveLine];
+        [self performSelector:@selector(solve) withObject:self afterDelay:1.0];
     }];
 }
 
-- (void)removeSubviews {
+-(void)solve {
+    [self solveMaze:self.startRow column:self.startCol];
+}
+
+-(BOOL)isSafe:(NSInteger)x y:(NSInteger)y {
+    if (x >= 0 && x < self.n*2 && y >= 0 && y < self.m*2 &&
+        [[[self.solArray objectAtIndex:x] objectAtIndex:y] integerValue] != 1 &&
+        [[[self.blockArray objectAtIndex:x] objectAtIndex:y] integerValue] == 1) {
+        return YES;
+    } else {
+        return NO;
+    }
+}
+
+
+-(BOOL)solveMaze:(NSInteger)row column:(NSInteger)column {
+    if ([[[self.blockArray objectAtIndex:row] objectAtIndex:column] integerValue] == 2) {
+        [[self.solArray objectAtIndex:row] replaceObjectAtIndex:column withObject:[NSNumber numberWithInt:1]];
+        [self drawSolveLine2];
+        return YES;
+    }
+    
+    if ([self isSafe:row y:column]) {
+        [[self.solArray objectAtIndex:row] replaceObjectAtIndex:column withObject:[NSNumber numberWithInt:1]];
+        //[self printArrayPretty:self.solArray];
+        
+        if ([self solveMaze:(row + 1) column:column]) {
+            return YES;
+        }
+        
+        if ([self solveMaze:row column:(column + 1)]) {
+            return YES;
+        }
+        
+        if([self solveMaze:row column:column-1]) {
+            return YES;
+        }
+        
+        if([self solveMaze:row-1 column:column]) {
+            return YES;
+        }
+        
+        [[self.solArray objectAtIndex:row] replaceObjectAtIndex:column withObject:[NSNumber numberWithInt:0]];
+        return NO;
+    }
+    
+    return NO;
+}
+
+-(void)printArrayPretty:(NSMutableArray*)array {
+    NSMutableString *rowString = [NSMutableString string];
+
+    for (int r = 0; r < self.n * 2 + 1 ; r++) {
+        [rowString setString:[NSString stringWithFormat:@"%d: ", r]];
+
+        for (int c = 0; c < self.m * 2 + 1 ; c++) {
+            [rowString appendFormat:@"%@ ", [[[array objectAtIndex:c] objectAtIndex:r] integerValue] == 1 ? @"*" : @" "];
+        }
+        NSLog(@"%@", rowString);
+    }
+}
+
+
+-(void)drawSolveLine {
+    NSInteger padding = 25;
+    NSInteger size = (self.view.frame.size.width - padding * 2) / (self.m * 2 + 1);
+    [self removeSubviews:2];
+    
+    for (int r = 0; r < self.n * 2 + 1 ; r++) {
+        for (int c = 0; c < self.m * 2 + 1 ; c++) {
+            if ([[[self.blockArray objectAtIndex:r] objectAtIndex:c] integerValue] == 1) {
+                UIView *block = [[UIView alloc] initWithFrame:CGRectMake(r*size + padding, c*size + padding*5, size, size)];
+                block.alpha = 0.5;
+                block.backgroundColor = [UIColor redColor];
+                block.tag = 2;
+                [self.view addSubview:block];
+            }
+        }
+    }
+}
+
+-(void)drawSolveLine2 {
+    NSInteger padding = 25;
+    NSInteger size = (self.view.frame.size.width - padding * 2) / (self.m * 2 + 1);
+    [self removeSubviews:3];
+    
+    for (int r = 0; r < self.n * 2 + 1 ; r++) {
+        for (int c = 0; c < self.m * 2 + 1 ; c++) {
+            if ([[[self.solArray objectAtIndex:r] objectAtIndex:c] integerValue] == 1) {
+                UIView *block = [[UIView alloc] initWithFrame:CGRectMake(r*size + padding, c*size + padding*5, size, size)];
+                block.alpha = 0.5;
+                block.backgroundColor = [UIColor yellowColor];
+                block.tag = 3;
+                [self.view addSubview:block];
+            }
+        }
+    }
+}
+
+- (void)removeSubviews:(NSInteger)tag {
     NSArray *viewsToRemove = [self.view subviews];
     for (UIView *v in viewsToRemove) {
-        if (v.tag == 1) {
+        if (v.tag == tag) {
             [v removeFromSuperview];
         }
     }
